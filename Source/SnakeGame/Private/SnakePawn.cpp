a0afb48bed1c7bbe439415cca3d109a69a9a2bb8
Starmoonknight <44838508+Starmoonknight@users.noreply.github.com>
2026-04-09 16:16:28 +0200

    Started the SnakePawn

--- Source/SnakeGame/Private/SnakePawn.cpp
+++ Source/SnakeGame/Private/SnakePawn.cpp
@@ -1,34 +1,88 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
+// SnakePawn.cpp
 #include "SnakePawn.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
+#include "Components//SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "InputAction.h"
+#include "InputMappingContext.h"
 
 // Sets default values
 ASnakePawn::ASnakePawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Collision Sphere is the root object
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Collision"));
+	SetRootComponent(CollisionSphere);
+	CollisionSphere->InitSphereRadius(50.f);
+
+	// attach the VisualMesh to the sphere
+	VisualMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualMesh"));
+	VisualMesh->SetupAttachment(CollisionSphere);
+
+	// attach the SpringArm to the sphere and set position settings
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(CollisionSphere);
+	SpringArm->TargetArmLength = 700.f;
+	SpringArm->SetRelativeRotation(FRotator(-55.0f, 0.0f, 0.0f));
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritRoll = false;
+	SpringArm->bInheritYaw = false;
+
+	// look into enable lagg for smooth spring arm turn
+
+	// attach the camera to the SpringArm
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
 void ASnakePawn::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+
+	// should set up PlayerController and Input mappings
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer())
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* InputSubsystem = LocalPlayer->GetSubsystem<
+				UEnhancedInputLocalPlayerSubsystem>())
+			{
+				if (DefaultMappingContext)
+				{
+					InputSubsystem->AddMappingContext(DefaultMappingContext, 0);
+				}
+			}
+		}
+	}
 }
 
 // Called every frame
 void ASnakePawn::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void ASnakePawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
+void ASnakePawn::Turn(const FInputActionValue& Value)
+{
+}
--- Source/SnakeGame/Private/SnakePawn.h
+++ Source/SnakeGame/Private/SnakePawn.h
@@ -1,13 +1,22 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+// SnakePawn.h
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "SnakePawn.generated.h"
 
+class UStaticMeshComponent;
+class USphereComponent;
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
-class ASnakePawn : public APawn
+class SNAKEGAME_API ASnakePawn : public APawn
 {
 	GENERATED_BODY()
 
@@ -19,11 +28,59 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+private:
+	void Turn(const FInputActionValue& Value);
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SnakeBody|Component",
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USphereComponent> CollisionSphere;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SnakeBody|Component",
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UStaticMeshComponent> VisualMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SnakeBody|Camera",
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<USpringArmComponent> SpringArm;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SnakeBody|Camera",
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UCameraComponent> Camera;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SnakeBody|Input",
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SnakeBody|Input",
+		meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<UInputAction> TurnAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SnakeBody|Movement", meta = (AllowPrivateAccess = "true"))
+	float BaseMoveSpeed = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "SnakeBody|Movement", meta = (AllowPrivateAccess = "true"))
+	float BaseTurnSpeed = 120.0f;
+
+	float TurnInput = 0.0f;
+
+
+	/*
+	//FTransform Transform;  =  Translation: FVector (X, Y, Z), Rotation: FQuat (Quaternion), Scale: FVector (X, Y, Z)  
+	
+	FTransform Transform;
+	
+	FVector CurrentPosition = FVector(0.0f, 0.0f, 0.0f);
+	FVector TargetPosition = FVector(0.0f, 0.0f, 0.0f);
+
+	// which one to keep? 
+	FRotator CurrentRotation = FRotator(0.0f, 0.0f, 0.0f);
+	FQuat CurrentRotationQuat = FQuat(0.0f, 0.0f, 0.0f, 0.0f);
+	*/
 };
