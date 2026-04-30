1984145d56fa06718fbb70ca846de2e9555f9db1
Starmoonknight <44838508+Starmoonknight@users.noreply.github.com>
2026-04-30 15:23:35 +0200

    Added the grids data asset scripts, not implemented yet in grid

---
+++ Source/SnakeGame/Public/GridSettingsDataAsset.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include  "GridSettingsTypes.h"
+#include  "GridSettingsDataAsset.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class SNAKEGAME_API UGridSettingsDataAsset : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Grid")
+	FGridSettings GridSettings;
+};
---
+++ Source/SnakeGame/Public/GridSettingsTypes.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GridDirectionTypes.generated.h"
+
+
+/**
+ * 
+ */
+USTRUCT(BlueprintType)
+struct FGridSettings
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, Category = "Grid|Settings",
+		meta = (AllowPrivateAccess = "true", ClampMin = "1"))
+	FIntPoint GridDimensions = FIntPoint(40, 40);
+
+	UPROPERTY(EditAnywhere, Category = "Grid|Settings",
+		meta = (AllowPrivateAccess = "true"))
+	FVector2D GridOrigin = FVector2D::ZeroVector;
+
+	UPROPERTY(EditAnywhere, Category = "Grid|Settings",
+		meta = (AllowPrivateAccess = "true"))
+	float GridWorldZ = 0.f;
+
+	UPROPERTY(EditAnywhere, Category = "Grid|Settings",
+		meta = (AllowPrivateAccess = "true", ClampMin = "1"))
+	int32 CellSize = 100;
+
+	UPROPERTY(EditAnywhere, Category = "Grid|Settings",
+		meta = (AllowPrivateAccess = "true"))
+	int32 RootSeed = 12345;
+};
